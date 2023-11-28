e56a0c607b91c074111c1bae77a9a5ecc4e1c523
Rian Alves Cadamuro <80922940+riicad@users.noreply.github.com>
2023-11-28 20:23:00 -0300

    jog da velha react

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import JogoDaVelha from './Components/JogoDaVelha/JogoDaVelha';
+
+function App() {
+  return (
+    <div className="App">
+      <JogoDaVelha/>
+    </div>
+  );
+}
+
+export default App;
